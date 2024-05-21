244fc7eb6b9406ff570298a0645b28ee1e3a2271
Erlandfatur <151422818+Erlandfatur@users.noreply.github.com>
2024-05-21 11:41:39 +0700

    upp

--- UTS_KEL6/Assets/Script/NewRotateScaler.cs
+++ UTS_KEL6/Assets/Script/NewRotateScaler.cs
@@ -5,47 +5,42 @@ using UnityEngine.UI;
 
 public class NewRotateScaler : MonoBehaviour
 {
-    public Slider RotateSlider;
-    public Slider ScaleSlider;
-    public Slider RotateXSlider;
+    public Slider RotateSlider; // Slider untuk rotasi Y (global)
+    public Slider ScaleSlider;  // Slider untuk skala
+    public Slider RotateXSlider; // Slider untuk rotasi X (lokal)
 
-    public float scaleMinValue;
-    public float scaleMaxValue;
-    public float rotYMinValue;
-    public float rotYMaxValue;
-    public float rotXMinValue;
-    public float rotXMaxValue;
+    public float scaleMinValue; // Nilai minimum skala
+    public float scaleMaxValue; // Nilai maksimum skala
+    public float rotYMinValue; // Nilai minimum rotasi Y (global)
+    public float rotYMaxValue; // Nilai maksimum rotasi Y (global)
+    public float rotXMinValue; // Nilai minimum rotasi X (lokal)
+    public float rotXMaxValue; // Nilai maksimum rotasi X (lokal)
 
+    private float currentYRotation;
+    private float currentXRotation;
 
-    // Start is called before the first frame update
     void Start()
     {
+        // Mengatur nilai minimum dan maksimum untuk skala
         ScaleSlider.minValue = scaleMinValue;
         ScaleSlider.maxValue = scaleMaxValue;
-
         ScaleSlider.onValueChanged.AddListener(ScaleSliderUpdate);
 
-
+        // Mengatur nilai minimum dan maksimum untuk rotasi Y (global)
         RotateSlider.minValue = rotYMinValue;
         RotateSlider.maxValue = rotYMaxValue;
-
         RotateSlider.onValueChanged.AddListener(RotateSliderUpdate);
 
+        // Mengatur nilai minimum dan maksimum untuk rotasi X (lokal)
         RotateXSlider.minValue = rotXMinValue;
         RotateXSlider.maxValue = rotXMaxValue;
+        RotateXSlider.onValueChanged.AddListener(RotateXSliderUpdate);
 
-        RotateXSlider.onValueChanged.AddListener(RotateSliderAtas);
-
-
-
-        
+        // Inisialisasi rotasi awal
+        currentYRotation = transform.eulerAngles.y;
+        currentXRotation = transform.localEulerAngles.x;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     void ScaleSliderUpdate(float value)
     {
         transform.localScale = new Vector3(value, value, value);
@@ -53,12 +48,20 @@ public class NewRotateScaler : MonoBehaviour
 
     void RotateSliderUpdate(float value)
     {
-        transform.localEulerAngles = new Vector3(transform.rotation.x, value, transform.rotation.z);
+        currentYRotation = value;
+        UpdateRotation();
     }
 
-    void RotateSliderAtas(float x)
+    void RotateXSliderUpdate(float value)
     {
-        transform.localEulerAngles = new Vector3(x, transform.rotation.y, transform.rotation.z);
-    }
+        currentXRotation = value;
+        UpdateRotation();
+    }
 
+    void UpdateRotation()
+    {
+        Quaternion globalYRotation = Quaternion.Euler(0, currentYRotation, 0);
+        Quaternion localXRotation = Quaternion.Euler(currentXRotation, 0, 0);
+        transform.rotation = globalYRotation * localXRotation;
+    }
 }
